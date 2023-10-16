89ffb0641ec3fb9a2cf6eda03572fc0100bc8e1d
MarahAbusaleh <marah.abusaleh12@gmail.com>
2023-10-16 22:22:30 +0300

    Marah Start

--- react_laravel_project_backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ react_laravel_project_backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -6,16 +6,14 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
+
+
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('image');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
@@ -24,11 +22,7 @@ return new class extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
+
     public function down()
     {
         Schema::dropIfExists('users');
