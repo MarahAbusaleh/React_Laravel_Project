e58b9aae987f5d34661522c7323d6e0baa8eba09
MarahAbusaleh <marah.abusaleh12@gmail.com>
2023-10-17 00:24:17 +0300

    Marah Update

---
+++ react_laravel_project_backend/resources/views/AdminDashboard/Pages/admin/index.blade.php
@@ -0,0 +1,13 @@
+@extends('AdminDashboard.Layout.master')
+@section('title', 'Admins')
+@section('content')
+
+    <div class="main-panel">
+        <div class="content-wrapper">
+            <div class="row">
+                <div class="col-md-12 ">
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
